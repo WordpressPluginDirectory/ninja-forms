4d1803b0c06c895f5125a752be63a8d498e87406
Justin Gardner <justin@rhynorater.com>
2025-04-03 06:02:12 -0500

    3.10.0

--- ninja-forms/vendor/composer/installed.php
+++ ninja-forms/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'saturday-drive/ninja-forms',
-        'pretty_version' => 'dev-a9ebf159f890c1f4ed28911644ca49d9449a9caa',
-        'version' => 'dev-a9ebf159f890c1f4ed28911644ca49d9449a9caa',
-        'reference' => 'a9ebf159f890c1f4ed28911644ca49d9449a9caa',
+        'pretty_version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+        'version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+        'reference' => '40cb306af2df88c27a79ace435892df01c282878',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'saturday-drive/ninja-forms' => array(
-            'pretty_version' => 'dev-a9ebf159f890c1f4ed28911644ca49d9449a9caa',
-            'version' => 'dev-a9ebf159f890c1f4ed28911644ca49d9449a9caa',
-            'reference' => 'a9ebf159f890c1f4ed28911644ca49d9449a9caa',
+            'pretty_version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+            'version' => 'dev-40cb306af2df88c27a79ace435892df01c282878',
+            'reference' => '40cb306af2df88c27a79ace435892df01c282878',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
